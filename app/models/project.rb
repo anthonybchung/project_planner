bd25a93f71f2993aba5e8e6d73fd90bf8a4dd5d2
abchung <anthony.b.chung@gmail.com>
2023-05-24 01:46:24 +1000

    model Project validation and attribute default

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,8 @@
 class Project < ApplicationRecord
+
+  attribute :opened, :datetime, default: Time.now
+
+  validates :name, presence: true, uniqueness: true
+  validates :vision, length: {maximum: 256}
+  validates :opened, presence:true
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # Attributes:
+  #   - name
+  #   - vision
+  #   - opened
+  #
+
+  fixtures :projects
+
+  test "name must not be null" do
+    project = projects(:one)
+    project.name = " "
+    assert_not project.valid?
+  end
+
+  test "name must be unique" do
+    project = projects(:one)
+    project2 = projects(:two)
+    project2.name = project.name
+    assert_not project2.valid?
+  end
+
+  test "vision must be less than 256 characters" do
+    project = projects(:one)
+    project.vision = "a"*257
+    assert_not project.valid?
+  end
+
+  test "opened must not be null" do
+    project = projects(:one)
+    project.opened = " "
+    assert_not project.valid?
+  end
 end
