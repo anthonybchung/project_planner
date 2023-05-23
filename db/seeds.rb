6575bd9a44c92f1733cd9a8c09339e3886dd42ba
abchung <anthony.b.chung@gmail.com>
2023-05-24 02:15:23 +1000

    seed: Project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# Projects
+
+# projects = [
+#   {name: "Project Planner", vision:"an app that assist in planning and tracking a project"},
+#   {name: "Car pooling",vision:"a web app that will assist students in car pooling for school excursions"},
+#   {name: "Weather",vision: "an app that will track your location and give you live weather update."},
+#   {name: "Travel Fuel",vision: "a terminal app that will track your car fuel usage"}
+# ]
+#
+# Project.create(projects)
