40893dd6dbb49ec189832cd0e36445709e6c48d3
abchung <anthony.b.chung@gmail.com>
2023-05-25 09:52:37 +1000

    use includes method instead of requestSubmit method

--- app/javascript/controllers/project_list_controller.js
+++ app/javascript/controllers/project_list_controller.js
@@ -2,7 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="project-list"
 export default class extends Controller {
+
+  static targets = ["project","searchName"]
+
   search(){
-    this.element.requestSubmit()
+
+    this.projectTargets.forEach((project)=>{
+      let projectName = project.querySelector('h2').textContent
+      if(projectName.includes(this.searchNameTarget.value)){
+        project.classList.remove('hidden')
+      }else{
+        project.classList.add('hidden')
+      }
+    })
+    // this.element.requestSubmit()
   }
 }
